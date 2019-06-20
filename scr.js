cb2c5403b705d56d8cf80bc39a1b3585335b87bd
nishantkr14 <50355345+nishantkr14@users.noreply.github.com>
2019-06-20 15:22:29 +0530

    Update scr.js

--- scr.js
+++ scr.js
@@ -2,7 +2,32 @@ var ra;
 
 function rand()
 {
-    randoma=6;
+    randoma=Math.floor(Math.random()*6+1);
+    var b=document.getElementByID("imgg");
+    if(randoma==1)
+    {
+        b.src="d1.png";
+    }
+    else if(randoma==2)
+    {
+        b.src="d2.png";
+    }
+   else if(randoma==3)
+    {
+        b.src="d3.png";
+    }
+else if(randoma==4)
+    {
+        b.src="d4.png";
+    }
+   else if(randoma==5)
+    {
+        b.src="d5.png";
+    }
+   else if(randoma==6)
+    {
+        b.src="d6.png";
+    }
   
 }
 function numRand(y)
